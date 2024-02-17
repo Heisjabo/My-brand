e42bf302e0f248af1d4b375d7f25df20f09275b4
heisjabo <jabojeanmarie5@gmail.com>
2024-02-17 17:02:15 +0200

    make the header sticky

--- app.js
+++ app.js
@@ -5,3 +5,35 @@ menu.onclick = () => {
 	menu.classList.toggle('bx-x');
 	navlist.classList.toggle('open');
 };
+
+
+window.onscroll = function() {myFunction()};
+
+let header = document.querySelector("nav");
+
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+	navlist.classList.remove("open");
+	menu.classList.remove("bx-x");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// tracking the active tab
+
+(function activeNav(){
+	const nav = document.querySelector('nav').querySelectorAll('a');
+
+nav.forEach(element => {
+	element.addEventListener("click", function(){
+		nav.forEach(a => a.classList.remove("active"));
+		this.classList.add("active");
+	})
+});
+
+})();
+
